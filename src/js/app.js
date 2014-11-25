681e75ed4ac02cbd7568ab774c02d3305607cda5
Jair Trejo <jair@jairtrejo.mx>
2014-11-25 01:26:31 -0600

    Fixes navbar in App/Non-app iOS Safari modes

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ var Question = require('./models/question');
 var QuestionView = require('./views/question');
 
 $(function(){
+    if(window.navigator.standalone){
+        $('nav').css('padding-top', '15px');
+        $('body').css('padding-top', '85px');
+    }
     var question = new Question();
     var questionView = new QuestionView(question);
     $('#app').append(questionView.render().$el);
