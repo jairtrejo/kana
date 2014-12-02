7bc9109252d53ccbe7e0c5abe62062c37b1e9add
Jair Trejo <jair@jairtrejo.mx>
2014-12-01 19:30:59 -0600

    Adds jasmine-jquery

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'browserify'],
+    frameworks: ['jasmine-jquery', 'jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
